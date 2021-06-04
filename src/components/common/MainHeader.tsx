ea9fbe6a04e3e3437077173f1062abbcdda223bd
Alfredo Arranz <alfredo.arranz.alvarado@nttdata.com>
2021-06-04 09:08:20 +0200

    Introducción de Analytics

--- src/components/common/MainHeader.tsx
+++ src/components/common/MainHeader.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
-export const MainHeader: React.FC = () => (
-    <AppBar position='static'>
-        <Toolbar>
-            <Link to="/" style={{color: '#ffffff', textDecoration: 'none', fontWeight: 'bold'}}>!toohaK</Link>
-        </Toolbar>
-    </AppBar>
-);
+export const MainHeader: React.FC = () => {
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            ReactGA.initialize(process.env.REACT_APP_GA_TRACKID || '');
+            ReactGA.pageview(window.location.pathname);
+        }
+    }, []);
+
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Link to="/" style={{color: '#ffffff', textDecoration: 'none', fontWeight: 'bold'}}>!toohaK</Link>
+            </Toolbar>
+        </AppBar>
+    );
+};
